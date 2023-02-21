2336e634d37a3b02f735d825211f33ecba6ac765
Misho Rostomashvili <Rostomashvili05@gmail.com>
2023-02-22 00:15:02 +0400

    Add N#2 function

--- code/script.js
+++ code/script.js
@@ -1,8 +1,9 @@
 "use strict";
 
 // N#1
+// თუ a და b განსხვავებული ტიპებია მაგრამ ერთი მნიშვნელობა აქვს, ფუნქცია "ტოლია" -ს დაგვიბრუნებს ორი უდრისის (==) გამო.
 const equal = function (a, b) {
-  if (a === b) {
+  if (a == b) {
     return "ტოლია";
   } else {
     return "არ არის ტოლი";
@@ -11,4 +12,17 @@ const equal = function (a, b) {
 
 console.log(equal(2, 3));
 console.log(equal("3", 3));
-console.log(equal("3", "3"));
+
+// N#2
+// ფუნქციას შეყვანილი ტემპერატურა გადაყავს ცელსიუსში
+const temperatureFarToCel = function (a) {
+  if (typeof a === typeof 1) {
+    return ((a - 32) * 5) / 9;
+  } else {
+    return false;
+  }
+};
+
+console.log(temperatureFarToCel(140));
+console.log(temperatureFarToCel("50"));
+console.log(temperatureFarToCel(-140));
